0a3f0582f72c376c59d0bbb7f4205dbeb286cca9
petyodelta <petyodelta@gmail.com>
2015-11-07 00:13:37 +0200

    Internal class Configuration changed to public

--- Telerickr/Telerickr.Data/Migrations/Configuration.cs
+++ Telerickr/Telerickr.Data/Migrations/Configuration.cs
@@ -2,7 +2,7 @@ namespace Telerickr.Data.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<TelerickrDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<TelerickrDbContext>
     {
         public Configuration()
         {
