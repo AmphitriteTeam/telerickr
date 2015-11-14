92f7f5c3ba94fb60035a1dfc974d76a471e862d5
petyodelta <petyodelta@gmail.com>
2015-11-14 13:22:30 +0200

    Added AlbumController, AlbumResponceModel and AlbumRequestModel. Changed:  Configure validation logic for passwords. Less Restrictive. Changed: Rout for login to /api/Account/Login

---
+++ Telerickr/Telerickr.Services/Controllers/AlbumsController.cs
@@ -0,0 +1,67 @@
+﻿namespace Telerickr.Services.Controllers
+{
+    using System.Web.Http;
+    using System.Linq;
+    using Data;
+    using Telerickr.Models;
+    using Models;
+
+    public class AlbumsController : ApiController
+    {
+        private readonly IRepository<Album> albums;
+        private readonly IRepository<User> users;
+
+        public AlbumsController()
+        {
+            var db = new TelerickrDbContext();
+            this.albums = new GenericRepository<Album>(db);
+            this.users = new GenericRepository<User>(db);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var result = this.albums
+                .All()
+                .Select(AlbumResponceModel.FromModel)
+                .ToList();
+
+            return this.Ok(result);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var result = this.albums
+                .All()
+                .Where(a => a.Id == id)
+                .Select(AlbumResponceModel.FromModel)
+                .ToList();
+
+            return this.Ok(result);
+        }
+
+        [Authorize]
+        public IHttpActionResult Post(AlbumRequestModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var currentUser = this.User.Identity.Name;
+            var creator = this.users
+                .All()
+                .FirstOrDefault(u => u.UserName == currentUser);
+
+            var newAlbum = new Album
+            {
+                Title = model.Title,
+                User = creator
+            };
+
+            this.albums.Add(newAlbum);
+            this.albums.SaveChanges();
+
+            return this.Ok(newAlbum.Id);
+        }
+    }
+}
---
+++ Telerickr/Telerickr.Services/Models/AlbumResponceModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using Telerickr.Models;
+
+namespace Telerickr.Services.Models
+{
+    public class AlbumResponceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public static Expression<Func<Album, AlbumResponceModel>> FromModel
+        {
+            get
+            {
+                return a => new AlbumResponceModel
+                {
+                    Id = a.Id,
+                    Title = a.Title
+                };
+            }
+        }
+    }
+}
