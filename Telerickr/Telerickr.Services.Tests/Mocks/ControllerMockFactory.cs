c1eadb659cb0cbda29a9e7d7add0d2749e207fce
mr.krustev <mr.krustev1994@gmail.com>
2015-11-19 21:48:04 +0200

    Extracted method for repeating logic.

--- Telerickr/Telerickr.Services.Tests/Mocks/ControllerMockFactory.cs
+++ Telerickr/Telerickr.Services.Tests/Mocks/ControllerMockFactory.cs
@@ -6,62 +6,45 @@
     using Controllers;
     using Data;
     using Telerickr.Models;
+    using System.Web.Http;
 
     public static class ControllerMockFactory
     {
         public static AlbumsController GetAlbumsController(IRepository<Album> albums, IRepository<User> users, IRepository<Photo> photos, bool withUser = true, bool validUser = true)
         {
             var controller = new AlbumsController(albums, users, photos);
-            if (withUser)
-            {
-                var username = TestConstants.InvalidUsername;
-                if (validUser)
-                {
-                    username = TestConstants.ValidUsername;
-                }
-
-                controller.User = new GenericPrincipal(new GenericIdentity(username, "Passport"), new[] { "tester" });
-            }
-           
+            SetUser(controller, withUser, validUser);     
             return controller;
         }
 
         public static PhotosController GetPhotosController(IRepository<Photo> photos, IRepository<Album> albums, bool withUser = true, bool validUser = true)
         {
             var controller = new PhotosController(photos, albums);
-            if (withUser)
-            {
-                var username = TestConstants.InvalidUsername;
-                if (validUser)
-                {
-                    username = TestConstants.ValidUsername;
-                }
-
-                controller.User = new GenericPrincipal(new GenericIdentity(username, "Passport"), new[] { "tester" });
-            }
-            else
-            {
-                controller.User = null;
-            }
-
+            SetUser(controller, withUser, validUser);
             return controller;
         }
 
         public static CommentsController GetCommentsController(IRepository<Comment> comments, IRepository<Photo> photos, bool withUser = true, bool validUser = true)
         {
             var controller = new CommentsController(comments, photos);
-            if (withUser)
-            {
-                var username = TestConstants.InvalidUsername;
-                if (validUser)
-                {
-                    username = TestConstants.ValidUsername;
-                }
+            SetUser(controller, withUser, validUser);
+            return controller;
+        }
 
-                controller.User = new GenericPrincipal(new GenericIdentity(username, "Passport"), new[] { "tester" });
+        private static void SetUser(ApiController controller, bool withUser, bool validUser)
+        {
+            if (!withUser)
+            {
+                return;
             }
 
-            return controller;
+            var username = TestConstants.InvalidUsername;
+            if (validUser)
+            {
+                username = TestConstants.ValidUsername;
+            }
+
+            controller.User = new GenericPrincipal(new GenericIdentity(username, "Passport"), new[] { "tester" });
         }
     }
 }
