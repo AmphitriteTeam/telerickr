f145b02943d7985ac7f148d43c4ca868482fe6dd
Feram <feram@feram.co>
2016-09-01 22:16:51 +0000

    Fix typo begining

--- Telerickr/Telerickr.Services/resources/sammy/lib/plugins/sammy.googleanalytics.js
+++ Telerickr/Telerickr.Services/resources/sammy/lib/plugins/sammy.googleanalytics.js
@@ -74,7 +74,7 @@
     }
 
     this.helpers({
-      // Disable tracking for the current route. Put at the begining of the
+      // Disable tracking for the current route. Put at the beginning of the
       // route's callback
       noTrack: function() {
         disableTracking();
